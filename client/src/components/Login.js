35798e308402a8fa5c8c3d885f57e94c65f0b33e
Kelly Moreira <moreira.kd@outlook.com>
2019-09-20 12:45:04 -0400

    login working!!!

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,10 +1,13 @@
 import React from "react";
 import {Form, Field, withFormik } from 'formik';
+import axios from "axios";
 
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  
 
   return (
     <div className="login-form">
@@ -42,7 +45,20 @@ const FormikLogin = withFormik({
       username: username || '',
       password: password || ''
     }
+  },
+
+  handleSubmit(values, {props}) {
+    axios.post('http://localhost:5000/api/login', values)
+          .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            props.history.push('/bubbles')
+          })
+          .catch(err => {
+            console.log(err.response);
+          })
+
   }
+
 })(Login)
 
 
