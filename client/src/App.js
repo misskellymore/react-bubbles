df2603fb66157d39d6b6c6638eee8830e7df67a0
Kelly Moreira <moreira.kd@outlook.com>
2019-09-20 12:33:40 -0400

    private route and axiosWithAuth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute.js';
+import BubblePage from './components/BubblePage.js';
 
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
   const [colorList, setColorList] = useState([]);
+
+
   return (
     <Router>
       <div className="App">
@@ -14,6 +18,7 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute path='/bubbles' component={props => <BubblePage {...props} />} />
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Form, Field, withFormik } from 'formik';
 
+
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
